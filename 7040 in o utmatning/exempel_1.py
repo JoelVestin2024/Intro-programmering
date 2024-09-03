654d80846c42efc35ae2e9b3d1541b6b32bf4e2d
JoelVestin2024 <157116289+JoelVestin2024@users.noreply.github.com>
2024-09-03 15:19:40 +0200

    Update exempel_1.py

--- 7040 in o utmatning/exempel_1.py
+++ 7040 in o utmatning/exempel_1.py
@@ -2,4 +2,4 @@ namn = input("Vad heter du ditt as?")
 print("Hejsan svejsan", namn)
 
 färg = input("Vilken är den äckligaste färgen")
-print=("Det var en väldigt snygg", färg)
+print("Det var en väldigt snygg", färg)
